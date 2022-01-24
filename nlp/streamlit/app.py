43a6facedc7b1d2959e7be4f2c704b5acd770818
Joyce Tagal <joyce.tagal@gmail.com>
2022-01-24 13:15:31 -0800

    Updated app

--- nlp/streamlit/app.py
+++ nlp/streamlit/app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import gensim
 from gensim.corpora.mmcorpus import MmCorpus
 from gensim.test.utils import datapath
 from gensim.models import LdaSeqModel
